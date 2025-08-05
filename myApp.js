7a4c9f74dfaa48a9b628efcc1678cf03f96387da
creativebud <bernardpelgrim@gmail.com>
2025-08-04 18:38:49 -0700

    prevent cache

--- myApp.js
+++ myApp.js
@@ -9,6 +9,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 
 
